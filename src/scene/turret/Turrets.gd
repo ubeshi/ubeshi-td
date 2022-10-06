bdfad7b89fb3801571862a785e190419f4391d9e
anson-li <ansonli.94@gmail.com>
2022-10-05 21:42:49 -0600

    Add missile detection of enemies

--- src/scene/turret/Turrets.gd
+++ src/scene/turret/Turrets.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+var enemy_array = [];
+var built = false;
+var enemy;
+
 func _init():
 	# Runs when the scene initializes
 	pass;
 	
 func _ready():
-	# Whens when the scene loads (after init)
-	pass;
+	if built:
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"];
 	
 func _process(delta):
 	# Renders on every frame that has been rendered - depends on monitor refresh rate
@@ -15,8 +19,25 @@ func _process(delta):
 func _physics_process(delta):
 	# Runs on every physics frame (different than render)
 	# Edit this in Project -> Project Settings -> Physics
-	turn();
+	if enemy_array.size() != 0 and built:
+		select_enemy();
+		turn();
+	else:
+		enemy = null;
+	
+func select_enemy():
+	var enemy_progress_array = [];
+	for i in enemy_array:
+		enemy_progress_array.append(i.offset);
+	var max_offset = enemy_progress_array.max();
+	var enemy_index = enemy_progress_array.find(max_offset);
+	enemy = enemy_array[enemy_index];
 	
 func turn():
-	var enemy_position = get_global_mouse_position();
-	get_node("Turret").look_at(enemy_position);
+	get_node("Turret").look_at(enemy.position);
+
+func _on_Range_body_entered(body):
+	enemy_array.append(body.get_parent());
+
+func _on_Range_body_exited(body):
+	enemy_array.erase(body.get_parent());
