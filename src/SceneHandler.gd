2443f3b94a49e32c4489d78944c692d640a64514
anson-li <ansonli.94@gmail.com>
2022-10-05 22:10:14 -0600

    Fixed broken map tiles & minor lint changes from editor

--- src/SceneHandler.gd
+++ src/SceneHandler.gd
@@ -1,14 +1,14 @@
 extends Node
 
 func _ready():
-	get_node("MainMenu/Margin/VBoxContainer/NewGame").connect("pressed", self, "on_new_game_pressed");
-	get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", self, "on_quit_pressed");
-	
+    get_node("MainMenu/Margin/VBoxContainer/NewGame").connect("pressed", self, "on_new_game_pressed");
+    get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", self, "on_quit_pressed");
+    
 func on_new_game_pressed():
-	get_node("MainMenu").queue_free();
-	var game_scene = load("res://scene/main-scene/GameScene.tscn").instance();
-	add_child(game_scene);
-		
+    get_node("MainMenu").queue_free();
+    var game_scene = load("res://scene/main-scene/GameScene.tscn").instance();
+    add_child(game_scene);
+        
 func on_quit_pressed():
-	if !OS.has_feature('JavaScript'):
-		get_tree().quit();
+    if !OS.has_feature('JavaScript'):
+        get_tree().quit();
--- src/scene/enemies/blue_tank.gd
+++ src/scene/enemies/blue_tank.gd
@@ -6,22 +6,22 @@ var hp = 50;
 onready var health_bar = get_node("HealthBar");
 
 func _ready():
-	health_bar.max_value = 50;
-	health_bar.value = 50;
-	health_bar.set_as_toplevel(true);
+    health_bar.max_value = 50;
+    health_bar.value = 50;
+    health_bar.set_as_toplevel(true);
 
 func _physics_process(delta):
-	move(delta);
-	
+    move(delta);
+    
 func move(delta):
-	set_offset(get_offset() + speed * delta);
-	health_bar.set_position(position - Vector2(30, 30));
+    set_offset(get_offset() + speed * delta);
+    health_bar.set_position(position - Vector2(30, 30));
 
 func on_hit(damage):
-	hp -= damage;
-	health_bar.value = hp;
-	if (hp <= 0):
-		on_destroy();
-	
+    hp -= damage;
+    health_bar.value = hp;
+    if (hp <= 0):
+        on_destroy();
+    
 func on_destroy():
-	self.queue_free();
+    self.queue_free();
