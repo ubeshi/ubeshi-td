1b644af4c301565969c8bd7c3d02514b001a555c
Colin Tong <tongcolin@live.ca>
2022-10-12 22:21:47 -0400

    Tabs to spaces

--- src/SceneHandler.gd
+++ src/SceneHandler.gd
@@ -1,24 +1,24 @@
 extends Node
 
 func _ready():
-	load_main_menu();
+    load_main_menu();
 
 func load_main_menu():
-	get_node("MainMenu/Margin/VBoxContainer/NewGame").connect("pressed", self, "on_new_game_pressed");
-	get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", self, "on_quit_pressed");
+    get_node("MainMenu/Margin/VBoxContainer/NewGame").connect("pressed", self, "on_new_game_pressed");
+    get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", self, "on_quit_pressed");
 
 func on_new_game_pressed():
-	get_node("MainMenu").queue_free();
-	var game_scene = load("res://scene/main-scene/GameScene.tscn").instance();
-	game_scene.connect("game_finished", self, "unload_game");
-	add_child(game_scene);
+    get_node("MainMenu").queue_free();
+    var game_scene = load("res://scene/main-scene/GameScene.tscn").instance();
+    game_scene.connect("game_finished", self, "unload_game");
+    add_child(game_scene);
 
 func unload_game(result):
-	get_node("GameScene").queue_free();
-	var main_menu = load("res://scene/turret/MainMenu.tscn").instance();
-	add_child(main_menu);
-	load_main_menu();
+    get_node("GameScene").queue_free();
+    var main_menu = load("res://scene/turret/MainMenu.tscn").instance();
+    add_child(main_menu);
+    load_main_menu();
 
 func on_quit_pressed():
-	if !OS.has_feature('JavaScript'):
-		get_tree().quit();
+    if !OS.has_feature('JavaScript'):
+        get_tree().quit();
--- src/singleton/GameData.gd
+++ src/singleton/GameData.gd
@@ -1,24 +1,24 @@
 extends Node
 
 var tower_data = {
-	"GunT1": {
-		"damage": 20,
-		"rof": 0.3,
-		"range": 350,
-		"category": "Projectile",
-	},
-	"MissileT1": {
-		"damage": 100,
-		"rof": 3,
-		"range": 550,
-		"category": "Missile"
-	},
+    "GunT1": {
+        "damage": 20,
+        "rof": 0.3,
+        "range": 350,
+        "category": "Projectile",
+    },
+    "MissileT1": {
+        "damage": 100,
+        "rof": 3,
+        "range": 550,
+        "category": "Missile"
+    },
 }
 
 var ubeshi_color = {
-	"GREEN": "4EFF15",
-	"ORANGE": "E1BE32",
-	"RED": "E11E1E",
-	"GREEN_TRANSPARENT": "AD54FF3C",
-	"LIME_TRANSPARENT": "ADFF4545",
+    "GREEN": "4EFF15",
+    "ORANGE": "E1BE32",
+    "RED": "E11E1E",
+    "GREEN_TRANSPARENT": "AD54FF3C",
+    "LIME_TRANSPARENT": "ADFF4545",
 }
